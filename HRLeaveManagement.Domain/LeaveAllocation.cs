e2f26b7587a3ade293f4446136d9ee0098a94b5a
patrasxd <patrasxd@gmail.com>
2025-04-13 21:23:47 +0200

    Added queries and query handlers

--- HRLeaveManagement.Domain/LeaveAllocation.cs
+++ HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -5,6 +5,7 @@ namespace HRLeaveManagement.Domain;
 public class LeaveAllocation : BaseEntity
 {
     public int NumberOfDays { get; set; }
+#nullable enable
     public LeaveType? LeaveType { get; set; }
     public int LeaveTypeId { get; set; }
     public int Period { get; set; }
--- HRLeaveManagement.Domain/LeaveRequest.cs
+++ HRLeaveManagement.Domain/LeaveRequest.cs
@@ -7,10 +7,12 @@ public class LeaveRequest : BaseEntity
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 
+#nullable enable
     public LeaveType? LeaveType { get; set; }
     public int LeaveTypeId { get; set; }
 
     public DateTime DateRequested { get; set; }
+#nullable enable
     public string? RequestComments { get; set; }
 
     public bool? Approved { get; set; }
