fd4fdee34ba5c44d2f24d8dbd5e4c05030a366cf
patrasxd <patrasxd@gmail.com>
2025-04-13 19:55:50 +0200

    Add project files.

---
+++ HRLeaveManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace HRLeaveManagement.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
